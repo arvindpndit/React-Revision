c7e58b8ca6b708504e6975039e94f6cab3a5f3d1
arvindpndit <arvindpandit9808@gmail.com>
2023-05-07 09:07:16 +0530

    removed  "main": "App.js", from package.json

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 let root = ReactDOM.createRoot(document.querySelector("#root"));
 let heading = React.createElement(
