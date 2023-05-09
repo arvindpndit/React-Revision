78ded5e35e563846139b49e675ab66131af6a5b2
arvindpndit <arvindpandit9808@gmail.com>
2023-05-09 09:59:32 +0530

    solved siblings key error

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-let root = ReactDOM.createRoot(document.querySelector("#root"));
-let heading = React.createElement(
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+const heading1 = React.createElement(
   "h1",
   {
-    id: "heading",
-    props: "here we pass the props",
+    id: "heading1",
+    key: "1",
   },
-  "Hello Arvind"
+  "Heading one"
 );
-root.render(heading);
+
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "heading2",
+    key: "2",
+  },
+  "Heading two"
+);
+
+const container = React.createElement("div", {}, [heading1, heading2]);
+
+root.render(container);
