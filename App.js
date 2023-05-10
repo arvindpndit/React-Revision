23bb3837909e063ff530c902fe83e39f7710d968
arvindpndit <arvindpandit9808@gmail.com>
2023-05-10 21:13:53 +0530

    created a basic layout of the app

--- App.js
+++ App.js
@@ -3,12 +3,39 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
+/*
+Basic Layout of the application-
+ -AppLayout
+    -Navbar
+    -Body
+      -SearchBar
+      -RestaurantList
+        -RestaurantCard
+    -Footer
+*/
+
 const Navbar = () => {
   return <div>Navbar</div>;
 };
 
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div>
+      <RestaurantList />
+    </div>
+  );
+};
+
+const RestaurantList = () => {
+  return (
+    <div>
+      <RestaurantCard />
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return <div>RestaurantCard</div>;
 };
 
 const Footer = () => {
