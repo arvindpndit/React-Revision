26836f6db885de9b082c401838b0f2d152ba6c63
arvindpndit <arvindpandit9808@gmail.com>
2023-05-15 10:19:54 +0530

    useEffect -> fetched the real data from swiggy

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,29 @@
 import RestaurantList from "./RestaurantList";
 import SearchBar from "./SearchBar";
 import { cards } from "../constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const [newRestaurants, setNewRestaurant] = useState(cards);
+  //website loads => initial render => API call => update the ui with the new data
+  //useEffect (callback fn, dependency array)
+  useEffect(() => {
+    getRestaurantList();
+  }, []);
+
+  const getRestaurantList = async () => {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      setNewRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    } catch (e) {
+      console.log("error encountered" + { e });
+    }
+  };
+
+  console.log("render()");
   return (
     <div className="h-full w-9/12 mx-auto">
       <SearchBar cards={cards} setNewRestaurant={setNewRestaurant} />
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,7 +4,7 @@ const RestaurantList = ({ cards }) => {
   return (
     <div className=" mt-7 mb-10 flex flex-wrap justify-between gap-5 w-full mx-auto">
       {cards.map((card) => {
-        return <RestaurantCard restaurant={card} />;
+        return <RestaurantCard restaurant={card} key={card.data.id} />;
       })}
     </div>
   );
