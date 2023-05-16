6ea4343aac34851265bed21bdb65193f794491fb
arvindpndit <arvindpandit9808@gmail.com>
2023-05-16 07:49:00 +0530

    login/logour button built

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
+const userLoggedIn = () => {
+  //checking whether the user is logged in or not
+  //lets say it returns success
+  return false;
+};
+
 const Navbar = () => {
+  const [LoggedIn, setLoggedIn] = useState(false);
   return (
     <div className="bg-green-50">
       <div className="flex justify-between items-center py-3 bg-green-50 w-9/12 mx-auto rounded-xl ">
@@ -7,6 +16,25 @@ const Navbar = () => {
           <div>Home</div>
           <div>Service</div>
           <div>Cart</div>
+          {LoggedIn ? (
+            <button
+              className="px-2 py-1 bg-green-300"
+              onClick={() => {
+                setLoggedIn(false);
+              }}
+            >
+              Login
+            </button>
+          ) : (
+            <button
+              className="px-2 py-1 bg-green-300"
+              onClick={() => {
+                setLoggedIn(true);
+              }}
+            >
+              Logout
+            </button>
+          )}
         </div>
       </div>
     </div>
