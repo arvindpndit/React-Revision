da5fbb3572f1294001ab9a5052c181d976f7c773
arvindpndit <arvindpandit9808@gmail.com>
2023-06-01 08:38:51 +0530

    <UserContext.Consumer> in class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import ProfileClass from "./ProfileClass";
 import ProfileFunction from "./ProfileFunction";
 import React from "react";
+import UserContext from "../helper/userContext";
 
 class About extends React.Component {
   constructor(props) {
@@ -16,6 +17,18 @@ class About extends React.Component {
     return (
       <div className="h-screen w-9/12 mx-auto">
         <div className="mb-6">This is About Section 🚀</div>
+        <UserContext.Consumer>
+          {
+            /* {({ info }) => console.log(info)} */
+            ({ info }) => (
+              <div>
+                <h1>{info.name}</h1>
+                <h2>{info.email}</h2>
+                <h3>{info.linkedIn}</h3>
+              </div>
+            )
+          }
+        </UserContext.Consumer>
         <div className="flex justify-between ">
           {/* <ProfileFunction name={"Arvind"} />
         <ProfileClass name={"Arvind"} /> */}
